13f492d5e329ac1f456a01d4d6f590ab92c63391
Ramyajyothi09 <147421135+Ramyajyothi09@users.noreply.github.com>
2023-10-09 21:09:37 +0530

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,8 @@
+
+print('my name is P.Ramya Jyothi')
+print('my fname is P.Ravi shankar')
+print('my mname is P.Poorna')
+print('i am studying 2nd year Bsc.MPCS')
+print('my habbit is to learn new things')
+print('i am interested in doing coding')
+print('my favourite colour is blue')
